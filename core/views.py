02ef35a9e07ccee85a538f6515a61380347230c0
tiagobfaustino <tiagobfaustino@gmail.com>
2020-12-14 13:32:23 -0300

    Executado teste de templates

--- core/views.py
+++ core/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'curso': 'Programação Web com Django Framework'
+    }
+    return render(request, 'index.html', context)
 
 
 def contato(request):
