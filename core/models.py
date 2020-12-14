199928ff6ea2deae245d0cfe0854c19b07571a93
tiagobfaustino <tiagobfaustino@gmail.com>
2020-12-14 14:41:21 -0300

    Estudei models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    estoque = models.IntegerField('Qtde em Estoque')
+
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('Email', max_length=100)
