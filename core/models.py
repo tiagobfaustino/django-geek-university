996a18109c31d642fc5b0580e5335da9e9cebbc7
tiagobfaustino <tiagobfaustino@gmail.com>
2020-12-14 22:47:57 -0300

    Aprendi a usar o Django Shell e apresentar dados do banco de dados no template.

--- core/models.py
+++ core/models.py
@@ -5,8 +5,14 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Qtde em Estoque')
 
+    def __str__(self):
+        return self.nome
+
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('Email', max_length=100)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
